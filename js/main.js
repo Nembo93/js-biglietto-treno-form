17a5e4e7e144e7d35ae0910bfaa6650fa330c28e
nembo93 <francesco.c193@libero.it>
2022-10-21 14:52:08 +0200

    Aggiunge variabile attribuendole lo stesso valore di un input di html

---
+++ js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// VARIABILI
+// Nome passeggero
+let passenger_name = document.getElementById(`passenger`).value;
+console.log(passenger_name);
+// Tariffa a km
+const km_price=0.21;
+console.log(km_price);
+
