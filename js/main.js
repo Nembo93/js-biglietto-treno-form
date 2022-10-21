8c064fbffaa8cb78d43bca62f2fc9e0690451c68
nembo93 <francesco.c193@libero.it>
2022-10-21 15:16:04 +0200

    Dichiara prime variabili per costruzione algoritmo

--- js/main.js
+++ js/main.js
@@ -2,8 +2,23 @@
 
 // VARIABILI
 // Nome passeggero
-let passenger_name = document.getElementById(`passenger`).value;
-console.log(passenger_name);
+let passengerName = document.getElementById(`passenger`).value;
+console.log(passengerName);
+// Km da percorrere
+let tripLenght = document.getElementById(`trip_lenght_km`).value;
+console.log(tripLenght);
+// Sconto
+const minorDiscount = 0.2;
+const oldDiscount = 0.4;
+let discount="";
+let passengerAge = document.querySelector(`.passenger_age`).value;
+console.log(minorDiscount, oldDiscount, discount, passengerAge);
+
+
+
+
+
+
 // Tariffa a km
 const km_price=0.21;
 console.log(km_price);
