a01a239fc3b3aa171cc09c415ba4da6fad7d23be
nembo93 <francesco.c193@libero.it>
2022-10-21 16:30:12 +0200

    Aggiunge funzione addEvent Listener

--- js/main.js
+++ js/main.js
@@ -7,19 +7,44 @@ console.log(passengerName);
 // Km da percorrere
 let tripLenght = document.getElementById(`trip_lenght_km`).value;
 console.log(tripLenght);
+// Tariffa a km
+const kmPrice=0.21;
+console.log(kmPrice);
 // Sconto
 const minorDiscount = 0.2;
 const oldDiscount = 0.4;
 let discount="";
-let passengerAge = document.querySelector(`.passenger_age`).value;
-console.log(minorDiscount, oldDiscount, discount, passengerAge);
+// Bottone
+const ticketGenerator = document.getElementById(generator);
+
+// Prezzo iniziale biglietto
+
+
+// ALGORITMO
+
+
+
+ticketGenerator.addEventListener(`click`, function(){
+    let passengerAge = document.querySelector(`.passenger_age`).value;
+    console.log(minorDiscount, oldDiscount, discount, passengerAge);   
+    let price = kmPrice * tripLenght;
+    console.log(price);
+
+    if (passengerAge < 18){
+    price = kmPrice * tripLenght;
+    price -= price * minorDiscount;
+    console.log(price);
+    }
+    else if (passengerAge >= 65){
+    price = kmPrice * tripLenght;
+    price -= price * oldDiscount;
+    console.log(price);
+    }
+})
 
 
 
 
 
 
-// Tariffa a km
-const km_price=0.21;
-console.log(km_price);
 
